ee69dac46055829bb9aa2fac69a3290945519aec
lk <1776282511@qq.com>
2019-05-31 14:09:29 +0800

    加入全局less

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function addStyleResource(rule) {
         .loader('style-resources-loader')
         .options({
             patterns: [
-                path.resolve(__dirname, 'src/assets/css/extend.less')
+                path.resolve(__dirname, 'src/assets/css/common.less')
             ]
         })
 }
